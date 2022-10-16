f1053b58428e1748193169dffd97c18003aead83
Rahul <rahulpatil07109@gmail.com>
2022-10-17 00:23:06 +0530

    working on trending page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { Container } from "@mui/system";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import SimpleBottomNavigation from "./components/Nav/MainNav";
+import Movies from "./components/Pages/Movies/Movies";
+import Series from "./components/Pages/Series/Series";
+import Search from "./components/Pages/Search/Search";
+import Trending from "./components/Pages/Trending/Trending";
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <div className='app'>content </div>
+      <div className='app'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<Trending />} exact />
+            <Route path='/movies' element={<Movies />} />
+            <Route path='/series' element={<Series />} />
+            <Route path='/search' element={<Search />} />
+          </Routes>
+        </Container>
+      </div>
+
       <SimpleBottomNavigation />
-    </>
+    </BrowserRouter>
   );
 }
 
